561604b4ed8f554688aa50757f73397b9e818c2b
MDKershaw <m.kershaw@myron.finance>
2024-09-27 16:51:31 +0200

    Docker setup
    Co-authored-by: blabovic <blabovic@users.noreply.github.com>

--- apps/MediaFusion/src/app/app.controller.ts
+++ apps/MediaFusion/src/app/app.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
+  async getData() {
+    try {
+      const response = await axios.get('http://images:3000/api');
+      console.log('Response from http://images:3001/api');
+      console.log(response.data);
+    } catch (error) {
+      console.log('Error calling http://images:3001/api');
+      console.log(error.message);
+    }
     return this.appService.getData();
   }
 }
